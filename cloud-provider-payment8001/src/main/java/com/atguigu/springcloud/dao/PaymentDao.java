4834c34f73520e3c3a74b836e7796c65fa32fd1f
anpenghu <821171531@qq.com>
2020-07-10 11:06:14 +0800

    学习

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.dao;
+
+import com.atguigu.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface PaymentDao {
+	@Options(useGeneratedKeys = true,keyProperty = "id")
+	@Insert("insert into payment(serial) values(#{serial})")
+	int create(Payment payment);
+	
+	@Select("select * from payment where id = #{id}")
+	Payment get(@Param("id") Long id);
+}
