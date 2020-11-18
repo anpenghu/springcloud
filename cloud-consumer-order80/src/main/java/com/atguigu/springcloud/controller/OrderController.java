6424f123d71ab58d47a30a88b7e4d6f792505ba1
an <821171531@qq.com>
2020-11-18 20:15:27 +0800

    删除test

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,9 +1,31 @@
 package com.atguigu.springcloud.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
+@Slf4j
 public class OrderController {
-
-
+	//private final static String PAYMENT_URL = "http://localhost:8001";//不集群
+	private final static String PAYMENT_URL = "http://cloud-payment-service";//集群
+	
+	@Resource
+	private RestTemplate restTemplate;
+	
+	@PostMapping("/consumer/payment")
+	private CommonResult<Payment> create(Payment payment){
+		log.info(payment.getSerial());
+		return restTemplate.postForObject(PAYMENT_URL + "/payment",payment, CommonResult.class);
+	}
+	
+	@GetMapping("/consumer/payment/{id}")
+	public CommonResult<Payment> getPaymentById(@PathVariable("id") long id){
+		log.info("查询3");
+		return restTemplate.getForObject(PAYMENT_URL + "/payment/"+id,CommonResult.class);
+	}
 }
